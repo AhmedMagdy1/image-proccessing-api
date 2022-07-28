bbeca4d61f1d28de6bd6945f43747d4a21c0d537
ahmed magdy <amagdy@tendegrees.sa>
2022-07-28 21:15:30 +0200

    test image proccesing function

--- src/tests/image.spec.ts
+++ src/tests/image.spec.ts
@@ -1,5 +1,7 @@
 import supertest from 'supertest';
 import app from '../index';
+import { ImageObject } from '../routes/api/images';
+import resizeImage from '../services/resize';
 
 const request = supertest(app);
 
@@ -25,4 +27,18 @@ describe('Test Image Endpoint', (): void => {
     );
     expect(response.status).toBe(200);
   });
+
+  it('Check Image Proccessing Functionality', async (): Promise<void> => {
+    const inputImagePath = './images/input/image1.jpg';
+    const outputImagePath = './images/output/image1_600_600.jpg';
+    const imageObject: ImageObject = {
+      name: 'image1',
+      width: 600,
+      height: 600,
+    };
+    expect(async () => {
+      await resizeImage(imageObject, inputImagePath, outputImagePath);
+  }).not.toThrow();
+});
+
 });
